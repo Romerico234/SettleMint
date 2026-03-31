5d7dd0490fb77b8ee0c9a34bffb3ad44c24b23d6
Romerico <mrh.jrd.234@gmail.com>
2026-03-31 17:43:37 -0400

    setup backend service

---
+++ settlemint-service/internal/db/postgres.go
@@ -0,0 +1,22 @@
+package db
+
+import (
+	"context"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func NewPostgresPool(databaseURL string) (*pgxpool.Pool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	config, err := pgxpool.ParseConfig(databaseURL)
+	if err != nil {
+		return nil, err
+	}
+
+	config.MaxConns = 10
+
+	return pgxpool.NewWithConfig(ctx, config)
+}
