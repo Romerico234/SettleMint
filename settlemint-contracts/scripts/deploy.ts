5a4758e626876a374fa4a428ebef09984197613d
Romerico <mrh.jrd.234@gmail.com>
2026-04-14 04:58:08 -0400

    Set up smart contracts and integrations to blockchain and ipfs

---
+++ settlemint-contracts/scripts/deploy.ts
@@ -0,0 +1,8 @@
+async function main() {
+  // Deployment logic will be added once the contracts are implemented.
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
